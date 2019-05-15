2d8c2c5b56c5c7f30f8e26baa16aea6a5d376a9f
Dan Byrne <dan@danielbyrne.net>
2019-05-15 21:27:53 +0100

    Initial commit

--- StringReverse/Program.cs
+++ StringReverse/Program.cs
@@ -6,7 +6,41 @@ namespace StringReverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var toReverse = "The cat sat on the mat.";
+                Console.WriteLine(ReverseString(toReverse));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        static string ReverseString(string inputStr, int maxLength = 255)
+        {
+            if(inputStr == null) throw new ArgumentNullException("String must not be null");
+            if(inputStr.Length > maxLength) throw new ArgumentException($"String must not be greater than {maxLength} characters in length");
+
+            int i = 0;
+            int j = inputStr.Length - 1;
+
+            char[] workingStr = inputStr.ToCharArray();
+
+            while(i < j)
+            {
+                char leftMostChar = workingStr[i];
+
+                workingStr[i] = workingStr[j];
+                workingStr[j] = leftMostChar;
+
+                i++;
+                j--;
+            }
+
+            return new string(workingStr);
         }
     }
 }
---
+++ StringReverse/README.md
@@ -0,0 +1,9 @@
+﻿## Approach for the string reversal algorythm
+
+My first pass at this was to turn the string into a character array, iterate over the array in reverse and then add each character to a new array.
+
+The approach I went with was to maintain two integers i, and j. i is set to the beginning of the string and J to the length of the string.
+The algorythm swaps characters at those respective positions until they meet in the middle meaning we only have to iterate over half of the string.
+
+I also added a maxLength default parameter to ensure that the when this method is used it cannot be passed a string that might cause the thread to block (max string length is 1GB in length in .NET) unless it was the programmers intention to parse an overly large string.
+We don't have to worry about UTF-16 and surrogate pairs in the reversal operation as the decoding has already been done into a char[] by the .NET framework and we're not reading bytes.
